e0e0601d96b808d79b79f5249cd71308417d61f9
Tsugami <yslan3000@gmail.com>
2020-06-06 13:48:50 -0300

    add items collect in create point page

--- frontend/scripts/create-point.js
+++ frontend/scripts/create-point.js
@@ -21,13 +21,15 @@ async function handleCities (event) {
   let citySelect = document.querySelector('select[name=city]');
   let stateSelect = document.querySelector('input[name=state]');
 
+  citySelect.innerHTML = '<option value="">Selecione a Cidade</option>  ';
+  citySelect.disabled = true;
+
   const indexSelectedState = event.target.selectedIndex;
   stateSelect.value = event.target.options[indexSelectedState].text;
 
   const stateId = event.target.value;
   const cities = await getCitiesByStateId(stateId)
-  
-
+    
   for (const city of cities) {
     citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
   }
@@ -39,3 +41,28 @@ populateUFs()
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", handleCities)
+
+const itemsToCollect = document
+  .querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+  item.addEventListener('click', handleSelectItem);
+}
+
+const selectedItems = [];
+function handleSelectItem(event) {
+  const itemLi = event.target;
+  const itemId = itemLi.dataset.id;
+
+  itemLi.classList.toggle('selected')
+
+  const alreadySelected = selectedItems
+    .indexOf(itemId);
+
+  if (alreadySelected >= 0) {
+    selectedItems.splice(alreadySelected, 1)
+  } else {
+    selectedItems.push(itemId)
+  }
+
+}
